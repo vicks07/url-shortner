31e1d7a9a717f313e93831df0d53a0f5540e55e9
Vikram <vikram.january@gmail.com>
2020-01-07 16:24:11 +0530

    Added a serialnumber field with autoincrement package

--- Models/ShortUrl.js
+++ Models/ShortUrl.js
@@ -1,13 +1,20 @@
 const mongoose = require('mongoose');
+const autoIncrement = require('mongoose-auto-increment');
 const Schema = mongoose.Schema;
 const connection = mongoose.createConnection('mongodb+srv://urlshort:nBEaYuUvvfXBXXRy@cluster0-bkxjo.mongodb.net/shortner?retryWrites=true&w=majority',{
     useNewUrlParser:true,
     useUnifiedTopology: true
 });
 
+autoIncrement.initialize(connection);
+
 const ShortUrlSchema = new Schema({
     url: String,
     code: String,
+    num: {
+        type:Number,
+        ref: 'SerialNo'
+    },
     count: Number
 },{
     collection: 'shorturls',
@@ -15,6 +22,13 @@ const ShortUrlSchema = new Schema({
         createdAt: 'created_at',
         updatedAt: 'updated_at'
     }
+});
+
+ShortUrlSchema.plugin(autoIncrement.plugin, {
+    model: 'ShortUrl', 
+    field: 'num',
+    startAt:3,
+    incrementBy:1
 });
 
 const ShortUrl = connection.model('ShortUrl',ShortUrlSchema);
